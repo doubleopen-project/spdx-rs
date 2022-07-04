12b89401c0292e865bd18b5e7cfbe413a2540f6c
Mikko Murto <mikko.murto@hhpartners.fi>
2022-07-04 15:30:44 +0300

    style: make clippy happy
    Signed-off-by: Mikko Murto <mikko.murto@hhpartners.fi>

--- src/parsers/tag_value.rs
+++ src/parsers/tag_value.rs
@@ -441,11 +441,9 @@ fn multiline_text<'a>(i: &'a str) -> IResult<&'a str, &str, VerboseError<&'a str
 
 /// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
 /// trailing whitespace, returning the output of `inner`.
-fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(
-    inner: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+fn ws<'a, F, O, E: ParseError<&'a str>>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
 where
-    F: Fn(&'a str) -> IResult<&'a str, O, E>,
+    F: Fn(&'a str) -> IResult<&'a str, O, E> + 'a,
 {
     delimited(multispace0, inner, multispace0)
 }
